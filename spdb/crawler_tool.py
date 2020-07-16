40988b6123b2517681ceb5987c7fb4f415cf0df3
Seven <52610264+seven102161@users.noreply.github.com>
2020-07-16 23:06:05 +0800

    add params

--- spdb/crawler_tool.py
+++ spdb/crawler_tool.py
@@ -16,7 +16,7 @@ class CrawlerTool(object):
         print('写入成功！')
 
     @staticmethod
-    def get_proxy(page):
+    def get_proxy(page=1):
         base_url = 'https://www.kuaidaili.com/free/inha/'
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36',
--- spdb/spdb_data_get.py
+++ spdb/spdb_data_get.py
@@ -5,31 +5,35 @@ import random
 from crawler_tool import CrawlerTool
 
 
-def main(min_, max_):
+def main(start_=1, end_=2):
     global info_list
-    global proxy_list
+    # global proxy_list
     url = 'https://per.spdb.com.cn/was5/web/search'
     headers = {
-        'Cookie': 'Hm_lvt_e3386c9713baeb4f5230e617a0255dcb=1594876094,1594876178; Hm_lpvt_e3386c9713baeb4f5230e617a0255dcb=1594876178; TSPD_101=08e305e14cab2800e1bc8af2866b369cd827d13c49865a99f5e3f67446a77a4bceb429f586a49cf3552e61ca7663e766:; JSESSIONID=6863EF990389664754CC7DEF1064B09F; TS01d02f4c=01ea722d2a0ac88b72233a68fdb30bf70b4ef6c395e1e6b274cafb88e3a66c45a1988e4b03b6e455c72a5bca8c1a350ea86bbbc548c6772b1ef4dca710dac5355333aa93f4',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate, br',
         'Host': 'per.spdb.com.cn',
         'Origin': 'https://per.spdb.com.cn',
-        'Referer': 'https://per.spdb.com.cn/bank_financing/cxpt/',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-origin',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.13 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+        'Referer': 'https://per.spdb.com.cn/bank_financing/cxpt/?TSPD_101=08e305e14cab28001708bd6fadff00237c46c1757eee754f04284e78610bc42494dffd13aa59f66481c546a5b6452f2c%3a',
+        'Content-Length': '130',
         'X-Requested-With': 'XMLHttpRequest',
+        'Cookie': 'TS01d02f4c=01ea722d2aecb986ce4c6b7de590514941fdb89e69d77d578c2ab5400620dc56fa7e5d9406a2687454350467198ad348b344d5e504720f082e1a592c80327829c95b6a511f; JSESSIONID=FE31AA4607BA23361199992DD10E2367',
     }
-    for i in range(min_, max_):
+    for i in range(start_, end_):
         pg = str(i)
         params = {
             'metadata': 'pName|pCode|pType|pZpD|pGpS|pPub|pRisk|pPart|pMoney|pStatus|pForm',
             'channelid': '213326',
             'page': pg,
         }
-        proxies = random.choice(proxy_list)
+        # proxies = {
+        #     'http': random.choice(proxy_list),
+        # }
         try:
-            response = requests.post(url, headers=headers, params=params, proxies={'http': proxies})
+            # response = requests.post(url, headers=headers, data=data, proxies=proxies)
+            response = requests.post(url, headers=headers, params=params)
             original_data = response.text
             data_list = re.findall(r'{"pPub":".*?","pStatus":".*?","pPart":".*?","pName":".*?","pGpS":".*?","pCode":".*?","pZpD":".*?","pMoney":".*?","pRisk":".*?","pType":".*?","pForm":".*?"}', original_data, re.S)
             print(f'page {pg} get, show first line as below:\n', {data_list[0]})
@@ -42,8 +46,8 @@ def main(min_, max_):
 
 
 if __name__ == '__main__':
-    proxy_list = CrawlerTool.get_proxy(1)
-    print(proxy_list)
+    # proxy_list = CrawlerTool.get_proxy()
+    # print(proxy_list)
     info_list = []
-    main(1, 119)
-    CrawlerTool.write_to_json('spdb_data_original.json', info_list)
+    main()
+    # CrawlerTool.write_to_json('spdb_data_original.json', info_list)
