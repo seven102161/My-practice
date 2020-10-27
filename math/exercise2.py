3035653d41b3eeb05dc6399762fb784f113fa117
Seven <52610264+seven102161@users.noreply.github.com>
2020-10-28 00:10:46 +0800

    exercise2

---
+++ math/exercise2.py
@@ -0,0 +1,23 @@
+import random
+
+count = 25
+num = 0
+
+print('题目', ',', '答案')
+print()
+
+while count > 0:
+    x = random.randint(0, 9)
+    y = random.randint(0, 9)
+    result = x * y
+
+    sym_list = ['3', '4']
+    sym = random.choice(sym_list)
+    if sym == '3':
+        print(x, '*', y, '=', '______', ',', result)
+        print()
+    else:
+        print(result, '÷', x, '=', '______', ',', y)
+        print()
+    count -= 1
+
