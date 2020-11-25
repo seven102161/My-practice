c2ef400de52b8a8ff4a3413b64f0b5742c750a8b
Seven <52610264+seven102161@users.noreply.github.com>
2020-11-25 20:09:39 +0800

    优化排版

--- math/exercise3.py
+++ math/exercise3.py
@@ -4,23 +4,27 @@ import random
 def addition_subtraction(min_, max_):
     count = 25
 
-    print('题目', ',', '答案')
+    print('题目1', ',', '题目2', ',', '答案1', ',', '答案2')
     print()
 
     while count > 0:
-        x = random.randint(min_, max_)
-        y = random.randint(min_, max_)
-        if y > x:
+        x1 = random.randint(min_, max_)
+        y1 = random.randint(min_, max_)
+        x2 = random.randint(min_, max_)
+        y2 = random.randint(min_, max_)
+        if y1 > x1 or y2 > x2:
             continue
         sym_list = ['1', '2']
         sym = random.choice(sym_list)
         if sym == '1':
-            result = x + y
-            print(x, '+', y, '=', '______', ',', result)
+            result1 = x1 + y1
+            result2 = x2 + y2
+            print(x1, '+', y1, '=', '______', ',', x2, '+', y2, '=', '______', ',', result1, ',', result2)
             print()
         else:
-            result = x - y
-            print(x, '-', y, '=', '______', ',', result)
+            result1 = x1 - y1
+            result2 = x2 - y2
+            print(x1, '-', y1, '=', '______', ',', x2, '-', y2, '=', '______', ',', result1, ',', result2)
             print()
         count -= 1
 
@@ -28,29 +32,33 @@ def addition_subtraction(min_, max_):
 def multiplication_division(min_, max_):
     count = 25
 
-    print('题目', ',', '答案')
+    print('题目1', ',', '题目2', ',', '答案1', ',', '答案2')
     print()
 
     while count > 0:
-        x = random.randint(min_, max_)
-        y = random.randint(min_, max_)
+        x1 = random.randint(min_, max_)
+        y1 = random.randint(min_, max_)
+        x2 = random.randint(min_, max_)
+        y2 = random.randint(min_, max_)
 
-        result = x * y
+        result1 = x1 * y1
+        result2 = x2 * y2
 
         sym_list = ['3', '4']
         sym = random.choice(sym_list)
+
         if sym == '3':
-            print(x, 'x', y, '=', '______', ',', result)
+            print(x1, 'x', y1, '=', '______', ',', x2, 'x', y2, '=', '______', ',', result1, ',', result2)
             print()
         else:
-            print(result, '÷', x, '=', '______', ',', y)
+            print(result1, '÷', x1, '=', '______', ',', result2, '÷', x2, '=', '______', ',', y1, ',', y2)
             print()
         count -= 1
 
 
 if __name__ == '__main__':
-    # for _i in range(2):
-    #     addition_subtraction(0, 31)
+    for _i in range(2):
+        addition_subtraction(0, 31)
     for _i in range(2):
         multiplication_division(2, 9)
 
