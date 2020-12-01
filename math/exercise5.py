a96c897b619341905d65230e76187855a4db2d3f
Seven <52610264+seven102161@users.noreply.github.com>
2020-12-01 23:56:30 +0800

    更新： 1、exercise4，使用pandas写入excel方法 2、exercise5，使用xlwt写入excel方法，并增加字体大小与行高、列宽。

--- math/exercise5.py
+++ math/exercise5.py
@@ -1,5 +1,5 @@
 import random
-import pandas as pd
+import xlwt
 
 
 def addition_subtraction(min_, max_):
@@ -45,13 +45,9 @@ def ques_add():
     lst_ques3 = list()
     lst_ques4 = list()
     for _i in range(25):
-        # lst_ques1.append(' ')
         lst_ques1.append(addition_subtraction(3, 30)[0])
-        # lst_ques2.append(' ')
         lst_ques2.append(addition_subtraction(3, 30)[0])
-        # lst_ques3.append(' ')
         lst_ques3.append(addition_subtraction(3, 30)[0])
-        # lst_ques4.append(' ')
         lst_ques4.append(addition_subtraction(3, 30)[0])
 
     data = dict()
@@ -60,9 +56,7 @@ def ques_add():
     data['题目3'] = lst_ques3
     data['题目4'] = lst_ques4
 
-    index = [i + 1 for i in range(len(lst_ques1))]
-    df = pd.DataFrame(data=data, index=index)
-    return df
+    return data
 
 
 def ques_multi():
@@ -86,20 +80,44 @@ def ques_multi():
     data['题目3'] = lst_ques3
     data['题目4'] = lst_ques4
 
-    index = [i + 1 for i in range(len(lst_ques1))]
-    df = pd.DataFrame(data=data, index=index)
-    return df
+    return data
+
+
+def write_to_excel(sheet_name, ques_data):
+    global workbook
+    # 给工作表添加sheet表单
+    worksheet = workbook.add_sheet(sheet_name)
+    # 格式设置
+    style = xlwt.XFStyle()
+    # 设置字体样式
+    font = xlwt.Font()
+    font.name = '宋体'
+    font.height = 20 * 12
+    style.font = font
+    # 设置表头
+    head = ['题目1', '题目2', '题目3', '题目4']
+
+    # 将表头写入excel
+    for _j in range(len(head)):
+        worksheet.write(0, _j, head[_j], style)  # 第0行，第i列，第i列表头
+        # 设置列宽
+        worksheet.col(_j).width = 18 * 256
+
+    for _j in range(len(head)):
+        q_data = ques_data
+        q_list = q_data[head[_j]]
+        for _i in range(len(q_list)):
+            worksheet.write(_i + 1, _j, q_list[_i], style)
+            # 设置单元格高度
+            worksheet.row(_i + 1).height_mismatch = True
+            worksheet.row(_i + 1).height = 600
 
 
 if __name__ == '__main__':
-    df1 = ques_add()
-    print(df1)
-    df2 = ques_multi()
-    print(df2)
-    writer = pd.ExcelWriter('/Users/jiangzhiyi/Desktop/math.xlsx')
-    df1.to_excel(writer, columns=['题目1', '题目2', '题目3', '题目4'],
-                 index=False, encoding='utf-8', sheet_name='加滅法')
-    df2.to_excel(writer, columns=['题目1', '题目2', '题目3', '题目4'],
-                 index=False, encoding='utf-8', sheet_name='乘除法')
-    writer.save()
-    writer.close()
+    # 创建workbook
+    workbook = xlwt.Workbook(encoding='utf-8')
+    # 写入法减法sheet
+    write_to_excel('加减法', ques_add())
+    # 写入乘除法sheet
+    write_to_excel('乘除法', ques_multi())
+    workbook.save('/Users/jiangzhiyi/Desktop/math.xls')
